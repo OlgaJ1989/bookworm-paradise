ede5b7fe393bca436a670676e22a4b3ab6e22a52
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-02 14:09:50 +0000

    Register new models in admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
+""" File specyfying settings for the admin panel. """
 from django.contrib import admin
+from .models import Book, Genre, Review
 
-# Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Genre)
+admin.site.register(Review)
