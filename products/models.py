3ed970aea466e36ecc69817020315c84b2341d16
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-03 16:59:25 +0000

    Add genre links to navbar

--- products/models.py
+++ products/models.py
@@ -22,7 +22,9 @@ class Book(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100, null=True, blank=True)
     binding = models.CharField(max_length=100)
+    pages = models.CharField(max_length=100, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
--- products/views.py
+++ products/views.py
@@ -1,12 +1,34 @@
 """ File specyfying what views can be found in the app. """
-from django.shortcuts import render, get_object_or_404
-from .models import Book
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Book, Genre
 
 
 def all_books(request):
     """ A view generating the books, their sorting, and search queries """
     books = Book.objects.all()
-    context = {'books': books, }
+    query = None
+    genres = None
+    if request.GET:
+        if 'genre' in request.GET:
+            genres = request.GET['genre'].split(',')
+            books = books.filter(genre__name__in=genres)
+            genres = Genre.objects.filter(name__in=genres)
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('books'))
+            queries = Q(title__icontains=query) | Q(
+                description__icontains=query)
+            books = books.filter(queries)
+    context = {
+        'books': books,
+        'search_term': query,
+        'current_genres': genres,
+    }
     return render(request, 'products/books.html', context)
 
 
