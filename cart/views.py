634ca44a32470548ef307da3a2d5a4e2aaff61c8
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-10 14:04:44 +0000

    Add functionality to delete book from cart

--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,5 @@
 """ File storing the views for the cart app """
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_cart(request):
@@ -28,11 +28,23 @@ def modify_cart(request, item_id):
         of a specific book in the cart """
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart', {})
-
     if quantity > 0:
         cart[item_id] = quantity
     else:
         cart.pop(item_id)
-
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
+
+
+def delete_from_cart(request, item_id):
+    """ A view allowing to delete an item from the cart """
+    cart = request.session.get('cart', {})
+
+    del cart[item_id]
+    if not cart[item_id]:
+        cart.pop(item_id)
+    else:
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
