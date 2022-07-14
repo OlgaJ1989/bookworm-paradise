373a5019a87161ec54c6567bf94a8c162f010696
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-14 13:57:17 +0000

    Add styling to the review form labels

--- products/forms.py
+++ products/forms.py
@@ -7,3 +7,8 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ('title', 'body',)
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Name'}),
+            'body': forms.Textarea(
+                attrs={'placeholder': 'Enter description here'}),
+        }
