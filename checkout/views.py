3c1ab387fa0675f5b03ed89852c6c87f092057f0
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-16 19:49:42 +0000

    Create checkout.html template and checkout css directory

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not bag:
+        messages.error(request, "Your bag is empty!")
+        return redirect(reverse('books'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
