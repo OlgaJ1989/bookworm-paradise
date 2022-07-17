0a69ea523a7805fe12aef0f66f93f6d7f9a1b0fa
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-17 17:09:03 +0000

    Change stripe currency to gbp in settings.py

--- checkout/views.py
+++ checkout/views.py
@@ -9,7 +9,7 @@ from .forms import OrderForm
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     cart = request.session.get('cart', {})
     if not cart:
@@ -23,16 +23,19 @@ def checkout(request):
     intent = stripe.PaymentIntent.create(
         amount=stripe_total,
         currency=settings.STRIPE_CURRENCY,
-    ) 
-
-    print(intent)
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KgzUoBBe3oUHbWGInaYKXUFDDeT7Uf7IvrAat9iiAlDeIXXTp7oVYbfZ2OmX5bkfW4PTZ9YCeYsA6QFllEX4idC00KZ819FMN',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
