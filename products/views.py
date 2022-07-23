66d2a1bcdc4e1f3546f9d76bdec0b7c654b202c1
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-23 22:50:50 +0000

    Add authentication requirement to edit/delete review

--- products/views.py
+++ products/views.py
@@ -15,6 +15,7 @@ def all_books(request):
     genres = None
     sort = None
     direction = None
+
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
@@ -22,7 +23,6 @@ def all_books(request):
             if sortkey == 'title':
                 sortkey = 'lower_title'
                 books = books.annotate(lower_title=Lower('title'))
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -41,6 +41,7 @@ def all_books(request):
             queries = Q(title__icontains=query) | Q(
                 description__icontains=query)
             books = books.filter(queries)
+
     current_sorting = f'{sort}_{direction}'
     context = {
         'books': books,
@@ -48,6 +49,7 @@ def all_books(request):
         'current_genres': genres,
         'current_sorting': current_sorting,
     }
+
     return render(request, 'products/books.html', context)
 
 
@@ -56,7 +58,6 @@ def book_details(request, book_id):
     A view generating each book's details and the ReviewForm()
     allowing the user to add a book review
     """
-
     book = get_object_or_404(Book, pk=book_id)
     reviews = book.reviews.order_by("-posted_on")
 
@@ -77,7 +78,7 @@ def book_details(request, book_id):
     return render(request, 'products/book_details.html', context)
 
 
-@login_required
+@login_required(login_url='account_login')
 def add_book(request):
     """ Add a book to the store """
     if not request.user.is_superuser:
@@ -96,7 +97,6 @@ def add_book(request):
                     Please ensure the form is valid.')
     else:
         form = BookForm()
-
     template = 'products/add_book.html'
     context = {
         'form': form,
@@ -105,12 +105,13 @@ def add_book(request):
     return render(request, template, context)
 
 
-@login_required
+@login_required(login_url='account_login')
 def edit_book(request, book_id):
     """ Edit a book in the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that!')
         return redirect(reverse('home'))
+
     book = get_object_or_404(Book, pk=book_id)
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES, instance=book)
@@ -135,7 +136,7 @@ def edit_book(request, book_id):
     return render(request, template, context)
 
 
-@login_required
+@login_required(login_url='account_login')
 def delete_book(request, book_id):
     """ Delete a book from the store """
     if not request.user.is_superuser:
@@ -145,13 +146,20 @@ def delete_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     book.delete()
     messages.success(request, 'Book deleted!')
+
     return redirect(reverse('books'))
 
 
+@login_required(login_url='account_login')
 def edit_review(request, review_id):
     """ Edit a book review """
-
     review = get_object_or_404(Review, pk=review_id)
+
+    if request.user != review.author:
+        messages.error(
+            request, "You are not authorised to edit this review!")
+        return redirect('account_login')
+    
     if request.method == 'POST':
         form = ReviewForm(request.POST, request.FILES, instance=review)
         if form.is_valid():
@@ -176,14 +184,16 @@ def edit_review(request, review_id):
     return render(request, template, context)
 
 
-@login_required
+@login_required(login_url='account_login')
 def delete_review(request, review_id):
     """ View allowing users do delete existing reviews. """
     review = Review.objects.get(pk=review_id)
+
     if request.user != review.author:
         messages.error(
             request, "You are not authorised to delete this review!")
         return redirect('account_login')
+
     if request.method == 'GET':
         review.delete()
         messages.success(
