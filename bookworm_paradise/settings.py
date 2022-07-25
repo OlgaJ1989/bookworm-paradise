ae39e3979e7fde6477b0f7461dbbce947e50db06
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-25 18:00:16 +0000

    Change SECRET_KEY setting in settings.py

--- bookworm_paradise/settings.py
+++ bookworm_paradise/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-a4x2tuw9a#=fxt!3kn(91sms9l%2=@yuujwlvt6sq)pjsznuk^')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['bookworm-paradise.herokuapp.com', 'localhost']
 
