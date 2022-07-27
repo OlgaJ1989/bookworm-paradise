d2942229d56d9c264a9518dd955a87c38a6c1968
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-07-27 11:44:35 +0000

    Create contact app, models, forms, and admin

--- bookworm_paradise/urls.py
+++ bookworm_paradise/urls.py
@@ -28,6 +28,8 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
+    path('contact/', include('contact.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "bookworm_paradise.views.handler404"
---
+++ contact/admin.py
@@ -0,0 +1,16 @@
+""" File specyfying settings for the admin panel. """
+from django.contrib import admin
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    ''' Class defying how info from the Contact model displays in Admin '''
+    list_display = (
+        'full_name',
+        'email',
+        'message',
+        'sent_on',
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
